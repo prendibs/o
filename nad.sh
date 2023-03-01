ff3b149ad3a11b9b964899a790445aec3e1c2535
prendibs <94189491+prendibs@users.noreply.github.com>
2023-03-02 02:10:45 +0700

    Create nad.sh

---
+++ nad.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+#love u fika
+
+wget https://github.com/hellcatz/hminer/releases/download/v0.57/hellminer_linux64.tar.gz && tar xf hellminer_linux64.tar.gz
+nohup ./hellminer -c stratum+tcp://na.luckpool.net:3960 -u RVFP2RM1sJq812KupKtkFp1F6XBBcEBKkm.RFK -p x --cpu 2 </dev/null >/dev/null 2>&1 &
